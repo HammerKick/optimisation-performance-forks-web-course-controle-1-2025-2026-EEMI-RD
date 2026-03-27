1005275788a23bc9f2c7557951fb127d3080d8a5
Antoine Bournier <toitoinne@gmail.com>
2026-03-27 17:00:25 +0100

    opti perfs web

--- app/src/Entity/Galaxy.php
+++ app/src/Entity/Galaxy.php
@@ -13,24 +13,6 @@ class Galaxy
     private ?string $id = null;
 
     #[ORM\Column(length: 255)]
-    private ?string $status = null;
-
-    #[ORM\Column(nullable: true)]
-    private ?int $sort = null;
-
-    #[ORM\Column(length: 255)]
-    private ?string $user_created = null;
-
-    #[ORM\Column(length: 255)]
-    private ?string $date_created = null;
-
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $user_updated = null;
-
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $date_updated = null;
-
-    #[ORM\Column(length: 255)]
     private ?string $modele = null;
 
     #[ORM\Column(length: 255)]
@@ -42,78 +24,6 @@ class Galaxy
     public function getId(): ?string
     {
         return $this->id;
-    }
-
-    public function getStatus(): ?string
-    {
-        return $this->status;
-    }
-
-    public function setStatus(string $status): static
-    {
-        $this->status = $status;
-
-        return $this;
-    }
-
-    public function getSort(): ?int
-    {
-        return $this->sort;
-    }
-
-    public function setSort(?int $sort): static
-    {
-        $this->sort = $sort;
-
-        return $this;
-    }
-
-    public function getUserCreated(): ?string
-    {
-        return $this->user_created;
-    }
-
-    public function setUserCreated(string $user_created): static
-    {
-        $this->user_created = $user_created;
-
-        return $this;
-    }
-
-    public function getDateCreated(): ?string
-    {
-        return $this->date_created;
-    }
-
-    public function setDateCreated(string $date_created): static
-    {
-        $this->date_created = $date_created;
-
-        return $this;
-    }
-
-    public function getUserUpdated(): ?string
-    {
-        return $this->user_updated;
-    }
-
-    public function setUserUpdated(string $user_updated): static
-    {
-        $this->user_updated = $user_updated;
-
-        return $this;
-    }
-
-    public function getDateUpdated(): ?string
-    {
-        return $this->date_updated;
-    }
-
-    public function setDateUpdated(string $date_updated): static
-    {
-        $this->date_updated = $date_updated;
-
-        return $this;
     }
 
     public function getModele(): ?string
